70047ef45fc99e1c5b154826ce67af49d92fa866
Joshua Robison <joshua@paperspace.com>
2022-09-20 09:37:34 -0400

    Updated web UI

--- app.py
+++ app.py
@@ -12,33 +12,150 @@ from torch import autocast
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-# !pip install --upgrade diffusers transformers scipy ftfy
-# !pip install flax==0.5.0 --no-deps
-# !pip install ipywidgets msgpack rich 
-# import torch
-# import os
+st.set_page_config(
+    page_title="Stable Diffusion Image Generator",
+    page_icon="🎈",
+    layout='wide'
+)
+
+def _max_width_():
+    max_width_str = f"max-width: 4000px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
 
 
-# from preprocess import imgToTensor
-# from resnet import resnet18
-# import os
+_max_width_()
 
-# classes = ('plane', 'car', 'bird', 'cat',
-#         'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+ca, cb, cc = st.columns([1, 7, 1])
 
-# MODEL_DIR = '/opt/models/'
-# for filename in os.listdir(MODEL_DIR):
-#     if filename[-4:] == '.pth':
-#         filepath = os.path.join(MODEL_DIR,filename)
-# MODEL_PATH = filepath
+# with c30:
+    # st.image("logo.png", width=400)
+with cb:
+    st.title("Stable Diffusion Image Generator")
+    st.header("")
 
-pipe = joblib.load('/opt/models/pipeline-gpu.pkl')
-device = "cuda"
-pipe = pipe.to(device)
 
-st.title("Stable Diffusion Image Generator")
 
-st.text_input('Text prompt', key='prompt')
+    with st.expander("ℹ️ - About this app", expanded=True):
+
+        st.write(
+            """     
+        The *Stable Diffusion Image Generator* app is an easy-to-use interface built in Streamlit that allows you to enter in a prompt and view AI generated images!
+            """
+        )
+
+        st.markdown("")
+
+    st.markdown("")
+    st.markdown("## Generation Form")
+
+    # !pip install --upgrade diffusers transformers scipy ftfy
+    # !pip install flax==0.5.0 --no-deps
+    # !pip install ipywidgets msgpack rich 
+    # import torch
+    # import os
+
+
+    # from preprocess import imgToTensor
+    # from resnet import resnet18
+    # import os
+
+    # classes = ('plane', 'car', 'bird', 'cat',
+    #         'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # MODEL_DIR = '/opt/models/'
+    # for filename in os.listdir(MODEL_DIR):
+    #     if filename[-4:] == '.pth':
+    #         filepath = os.path.join(MODEL_DIR,filename)
+    # MODEL_PATH = filepath
+
+    # @st.cache
+    pipe = joblib.load('/opt/models/pipeline-gpu.pkl')
+    device = "cuda"
+    pipe = pipe.to(device)
+
+    # st.title("Stable Diffusion Image Generator")
+
+
+    def generate_image():
+        prompt = st.session_state.prompt
+        with autocast("cuda"):
+            image = pipe(prompt, guidance_scale=7.5, height=512, width=512,
+                           num_inference_steps=50, seed='random', scheduler='LMSDiscreteScheduler')["sample"][0]
+        # if prompt=='bird':
+        #     image_name = 'bird.jpeg'
+        # else: 
+        #     image_name = 'cat.jpeg'
+
+        image_name = 'generated_image.jpeg"'
+        image = Image.open(image_name)
+
+        from io import BytesIO
+        buf = BytesIO()
+        image.save(buf, format="JPEG")
+        byte_im = buf.getvalue()
+
+
+        # st.image(image, caption='Generated image.', use_column_width=True)
+        return image, byte_im, image_name
+
+        # btn.data=byte_im
+        # btn.file_name=image_name
+
+    # ce, c1, ce, c2, c3 = st.columns([0.07, 1, 0.07, 5, 0.07])
+    # cs, c1, c2, c3, cLast = st.columns([2, 1.5, 1.5, 1.5, 2])
+c1, c2, c3, c4 = st.columns([1, 3.5, 3.5, 1])
+with c2:
+    with st.form(key="my_form"):
+        
+            size = st.radio(
+                "Choose image size",
+                ["Small", "Medium", "Large"],
+            )
+
+            num_inference_steps = st.slider(
+                "# of generation steps",
+                min_value=10,
+                max_value=100,
+                value=50,
+                help="You can choose the number of inference steps the model will take during image generation. Between 10 and 100, default number is 50.",
+            )
+
+            submit_button = st.form_submit_button(label="Generate")
+                # generateButton.disabled=True
+with c3:
+    st.text_area(label='Text prompt', value='Enter in your text prompt', key='prompt', height=260)
+
+if not submit_button:
+    st.stop()
+
+c1, c2, c3, c4, c5 = st.columns([1, 5, .3, 1, 1])
+image, byte_im, image_name = generate_image()
+with c2:
+    st.image(image, caption='Generated image.', use_column_width=True)
+with c4:
+    btn = st.download_button(
+        label="Download Image",
+        data=byte_im,
+        file_name=image_name,
+        mime="image/jpeg",
+        )
+
+# st.write(size)
+
+            # generateButton.enabled=False
+
+        # if ModelType == "Default (DistilBERT)":
+            # kw_model = KeyBERT(model=roberta)
+
 # image = Image.open('bird.jpeg')
 
 
@@ -76,25 +193,26 @@ st.text_input('Text prompt', key='prompt')
 # You can access the value at any point with:
 # st.session_state.name
 
-def generate_image():
-    prompt = st.session_state.prompt
-    with autocast("cuda"):
-        image = pipe(prompt, guidance_scale=7.5, height=512, width=512,
-                       num_inference_steps=50, seed='random', scheduler='LMSDiscreteScheduler')["sample"][0]
-    # if prompt=='bird':
-    #     image = Image.open('bird.jpeg')
-    # else: 
-    #     image = Image.open('cat.jpeg')
-        
-    st.image(image, caption='Generated image.', use_column_width=True)
+
+
     # sleep(5)
+
+
+# byte_im =''
+# image_name=''
+
+
+# btn = st.download_button(
+#     label="Download Image",
+#     data=byte_im,
+#     file_name=image_name,
+#     mime="image/jpeg",
+#     )
 
 # st.button('Generate', on_click=generate_image)
 # generateButton = st.button("Generate", DISABLED=True)
-if st.button("Generate"):
-    # generateButton.disabled=True
-    generate_image()
-    # generateButton.enabled=False
+
+
 
 
 
